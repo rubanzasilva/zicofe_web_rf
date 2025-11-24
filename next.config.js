38e48833c6181aa5c202d21df9109a9d0995145b
rubanzasilva <abc@rubanzasilver.com>
2025-11-24 13:03:28 +0300

    adding logo

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     // Configure domains if you need to use external image sources
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'd322abd807wvow.cloudfront.net'],
   },
 }
 
